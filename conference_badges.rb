07a834220b3c0a5bc254efe4c22ec174ccbedc37
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-07 02:34:53 +0300

    Done with lab test

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    badges = []
+    names.each do |name|
+      badge = badge_maker(name)
+      badges << badge
+    end
+    badges
+end
+  
+#binding.pry
